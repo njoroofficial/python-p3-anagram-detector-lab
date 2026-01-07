4cf3ee1b420473392849dd740f198532fd3524f4
njoroofficial <mosesn579@gmail.com>
2026-01-07 14:49:15 +0300

    anagram challenge

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,25 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self._sorted_word = sorted(self.word)
+
+    def match(self, words):
+        matchingList = []
+
+        for word in words:
+            lower_word = word.lower()
+
+            if lower_word == self.word:
+                continue
+
+            if self._sorted_word == sorted(lower_word):
+                matchingList.append(word)
+
+        return matchingList
+
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
+
+
+
